55ddaed945884d6d06faf752a8572d55b76a102f
Erick Guimaraes <erickgdeoliveira@gmail.com>
2020-09-24 04:37:34 -0300

    fixing typos of index text

--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ app.use(planetsRoutes);
 app.get("/", (req, res) =>
 {
   res.send(`Star wars Api Developed By Erick Oliveira` + 
-  `<br> To See all planets go to /Planets with a Get request` + 
-  `<br> To See a specific planet go to /planet/ID with a Get request` +
+  `<br> To Find all planets go to /Planets with a Get request` + 
+  `<br> To Find a specific planet go to /planet/ID with a Get request` +
   `<br> To Delete a planet go to /Planets/ID  with a Delete request` + 
   `<br> To Create a planet send a Put request to /planet/ and send a Json with` +
   `<br> <pre>{`+
