dfa8e2633f78910fbe0902bc4066e4d33b3a04fc
Erick Guimaraes <erickgdeoliveira@gmail.com>
2020-09-28 07:15:50 -0300

    Adding tests and fixing positions of brackets

--- src/db/index.js
+++ src/db/index.js
@@ -1,30 +1,29 @@
-const dotenv = require ("dotenv");
-const mongoose = require ("mongoose");
-const configuration = require(`../config/index.js`)
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const configuration = require(`../config/index.js`);
 
 var DbConfigConnection = configuration.DB_TEST_CONNECTION;
-var DbConnectedText = "Test Database"
-if(process.argv[2] == "dev")
-{
-    DbConfigConnection = configuration.DB_CONNECTION;
-    DbConnectedText = "Dev Database"
+var DbConnectedText = "Test Database";
+if (process.argv[2] == "dev") {
+  DbConfigConnection = configuration.DB_CONNECTION;
+  DbConnectedText = "Dev Database";
 }
 
-const  dbConnection = 
-
-mongoose.connect(DbConfigConnection,
+const dbConnection = mongoose.connect(
+  DbConfigConnection,
   {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true} ,
-    (err)=> 
-{
-    if(err)
-    {
-        console.log("An error was found while trying to connect the data base, and the error was " + err);
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) {
+      console.log(
+        "An error was found while trying to connect the data base, and the error was " +
+          err
+      );
+    } else {
+      console.log("Connected to " + DbConnectedText);
     }
-    else{
-    console.log("Connected to " + DbConnectedText)
-    }
-});
-module.exports = dbConnection
-
+  }
+);
+module.exports = dbConnection;
